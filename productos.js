22a79ab214d4d9d53638b383a78c6070a033aed7
Enzo Soliz <enzosoliz95@gmail.com>
2023-10-27 12:30:18 -0300

    Modificacion de productos: se agrega el archivo pastas.json. Creamos el carrito de compras

--- productos.js
+++ productos.js
@@ -1,26 +1,26 @@
-const pastas = [
-  {
-    id: 1,
-    foto: "../assets/img/fideos.jpg",
-    nombre: "Fideos",
-    precio: 1600,
-  },
-  {
-    id: 2,
-    foto: "../assets/img/fideosVerdes.jpg",
-    nombre: "Fideos de Espinaca",
-    precio: 1800,
-  },
-  {
-    id: 3,
-    foto: "../assets/img/ravioles.jpg",
-    nombre: "Ravioles",
-    precio: 2100,
-  },
-  {
-    id: 4,
-    foto: "../assets/img/ñoquis.jpg",
-    nombre: "Ñoquis",
-    precio: 1900,
-  },
-];
+/* const fs = import('fs');
+
+function importarProductos() {
+  // Definimos la ruta del archivo
+  const rutaArchivo = './data/pastas.json';
+  // Verificamos que el archivo exista
+  if (!fs.existsSync(rutaArchivo)) {
+    throw new Error('El archivo no existe');
+  }
+  // Leemos el archivo y lo parseamos a JSON
+  const contenido = JSON.parse(fs.readFileSync(rutaArchivo, 'utf8'));
+  return contenido
+}
+
+module.exports = { importarProductos }; */
+function importarProductos() {
+  const rutaArchivo = '../data/pastas.json';
+  return fetch(rutaArchivo)
+    .then(response => response.json()) //indicamos el formato en que queremos obtener la informacion
+    .then(data => data); //data es el objeto que nos devuelve la promesa
+
+  
+
+}
+
+export { importarProductos };
